7b569245d7d8460a39073c0bb7db3091b6832d73
GG <723687948@qq.com>
2017-10-16 15:17:21 +0800

    ScrollView添加下拉刷新。

--- src/app/ScrollView/index.js
+++ src/app/ScrollView/index.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, ScrollView, RefreshControl } from 'react-native';
 
 export default class _ScrollView extends Component {
 
+  state = {
+    refreshing: false,
+    listData: [5, 4, 3, 2, 1]
+  };
+
+  _onRefresh() {
+    this.setState({ refreshing: true });
+
+    setTimeout(() => {
+      let listData = this.state.listData.concat();
+      for (let i = 0; i < 5; i++) {
+        listData.unshift(listData[0] + 1);
+      }
+
+      this.setState({
+        refreshing: false,
+        listData
+      });
+    }, 2000);
+  }
+
   render() {
-
-    var images = [];
-    for (let i = 0; i < 10; i++) {
-      images.push(<Image key={i} style={styles.image} source={require('../../../resource/assets/logo.png')} />);
-    }
-
     return (
-      <ScrollView contentContainerStyle={styles.container}>
+      <ScrollView contentContainerStyle={styles.container} refreshControl={
+        <RefreshControl title="正在刷新..." titleColor="#f00"
+                        refreshing={this.state.refreshing} onRefresh={this._onRefresh.bind(this)} />}>
         <Text style={styles.label}>Start</Text>
-        {images}
+        {this.state.listData.map(item => <Text style={styles.item} key={item}>{item}</Text>)}
         <Text style={styles.label}>End</Text>
       </ScrollView>
     );
@@ -26,13 +43,12 @@ const styles = StyleSheet.create({
     paddingVertical: 20
   },
   label: {
-    lineHeight: 50,
     fontSize: 50,
     textAlign: 'center'
   },
-  image: {
-    width: 300,
-    height: 300,
-    marginVertical: 20
+  item: {
+    margin: 50,
+    fontSize: 200,
+    textAlign: 'center'
   }
 });
